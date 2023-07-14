543a48da7069e2379e63a1f6dcf99afc6fdcac05
Tom Yordanov <tmy@Toms-MacBook-Pro.local>
2023-07-14 16:20:31 +0300

    validation fixing & delete user implementation

--- users/backend_logic.py
+++ users/backend_logic.py
@@ -1,6 +1,5 @@
 #here the whole logic for user creation and bla bla bla is going to be implemented
 from .models import User
-from .validators import user_name_validator, first_name_validator, last_name_validator, password_validator, email_validator
 from django.contrib.auth.hashers import make_password
 
 
@@ -21,14 +20,20 @@ def create_user(request) -> dict:
         return {'Error': 'the email must be unique'}
     if User.objects.filter(user_name=user_name).exists():
         return {'Error': 'the username must be unique'}
+    
+    #validations
+    if len(user_name) < 3:
+       return {'Error': 'the username is too short'}
+    if len(first_name) == 0:
+        return {'Error': 'the name field cannot be empty'}
+    if len(last_name) == 0:
+        return {'Error': 'the last_name field cannot be empty'}
 
-
-
-    user_name_validator(user_name)
-    first_name_validator(first_name)
-    last_name_validator(last_name)
-    password_validator(password, password_check)
-    email_validator(email)
+    
+    if password != password_check:
+        return {'Error': 'passwords do not match'}
+    if len(password) < 6:
+        return {'Error': 'the lenght of the password is too short'}
 
     # TODO: better regex to verify email, more password validations, put method for user, login method
     hashed_password = make_password(password)
@@ -40,4 +45,31 @@ def create_user(request) -> dict:
         first_name=first_name,
         last_name=last_name,
     )
-    return {'Sucess': 'asd'}
+    return {'Sucess': 'successfully created user'}
+
+
+def edit_user(request):
+    user_name = request.data.get('user_name')
+    email = request.data.get('email')
+    password = request.data.get('password')
+    password_check = request.data.get('password_check')
+    first_name = request.data.get('first_name')
+    last_name = request.data.get('last_name')
+
+    new_user_conf = User.objects.filter
+
+
+def delete_user_account(request):
+    """
+    Deletes user account based on the email
+    """
+    email = request.data.get('email')
+
+    if email:
+        try:
+            User.objects.get(email=email).delete()
+            return {'Success': 'account deletion was successful.'}
+        except User.DoesNotExist:
+            return {'Error': 'An account with that email does not exist'}
+    else:
+        return {'Error': 'the field cannot be blank'}
--- users/views.py
+++ users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .backend_logic import create_user
+from .backend_logic import create_user, delete_user_account
+
 
 class UserRegistration(APIView):
     """User registration"""
@@ -14,3 +15,12 @@ class UserRegistration(APIView):
         else:
             return Response(data={'Success': 'Created user'}, status=status.HTTP_200_OK)
         
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        msg = delete_user_account(request)
+        if 'Error' in msg.keys():
+            return Response(data=msg, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(data=msg, status=status.HTTP_200_OK)
