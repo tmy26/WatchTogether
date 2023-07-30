1d9fe18d445df16b23df02130011164a0f516d73
Vasil <stu2101321007@uni-plovdiv.bg>
2023-07-30 13:31:19 +0300

    typos-fixed

--- users/backend_logic_stream.py
+++ users/backend_logic_stream.py
@@ -1,6 +1,8 @@
-from .models import Room , Stream
+from .models import Room, Stream
 from django.core.exceptions import MultipleObjectsReturned ,ValidationError
 
+
+error_message = {"Error": "Something went wrong with the data you provided. Please check if the data is correct and try again."}
 
 def create_steam(request) -> dict:
     """Stream creation function"""
@@ -11,24 +13,23 @@ def create_steam(request) -> dict:
 
      #Check if assigned_room is provided and if the room exists
     if not assigned_room:
-        return {"Error": "Assigned room is required."}  
+        return {'Error': 'Assigned room is required.'}  
     try:
         #Check if the room exists
         room = Room.objects.get(room_unique_id=assigned_room)
     
         # Check if the room already has a stream assigned
         if Stream.objects.filter(assigned_room=room).exists():
-            return {"Error": "The room already has a stream assigned."}
+            return {'Error': 'The room already has a stream assigned.'}
         
         created_stream = Stream(link=link, assigned_room=room)
         created_stream.save() 
     except (Stream.DoesNotExist, Room.DoesNotExist, MultipleObjectsReturned, ValidationError):
-        return {"Error": "Something went wrong with the data you provided please check if the data is correct and try again."}
+        return error_message
     
     return {'Success': 'Stream created'}
     
     
-
 def edit_stream(request) -> dict:
     """Stream editing function"""
     link = request.data.get('link')
@@ -36,7 +37,7 @@ def edit_stream(request) -> dict:
 
     # Check if requested data is null
     if not link or not assigned_room:
-        return {"Error": "Invalid data. 'link' and/or 'assigned_room' is required."}
+        return error_message
     try:
         # Check if assigned_room is provided and if the room exists
         if assigned_room and Room.objects.filter(room_unique_id=assigned_room).exists():
@@ -45,7 +46,7 @@ def edit_stream(request) -> dict:
             stream_to_edit.link = link
             stream_to_edit.save()
     except (Stream.DoesNotExist, Room.DoesNotExist, MultipleObjectsReturned, ValidationError):
-        return {"Error": "Something went wrong with the data you provided please check if the data is correct and try again."}
+        return error_message
         
     return {'Success': 'Stream edited'}    
         
