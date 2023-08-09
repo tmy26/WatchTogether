e030b681f13b1d2dcdff3464f21e43c42f1c0257
Tom Yordanov <tmy@Toms-MacBook-Pro.local>
2023-08-09 13:52:13 +0300

    this actually is refactoring and not implementing new features

--- wt_mobile/urls.py
+++ wt_mobile/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import UserRegistration, EditUser, RoomCreation, StreamCreation
+from .views import UserRegistrationView, RoomCreationView, StreamCreationView
 from . import backend_logic
 
 
 urlpatterns = [
-    path('user_registration', UserRegistration.as_view()),
-    path('user_edit', EditUser.as_view()),
-    path('room_creation', RoomCreation.as_view()),
+    path('user_registration', UserRegistrationView.as_view()),
+    path('room_creation', RoomCreationView.as_view()),
     path('activate/<uidb64>/<token>', backend_logic.activate, name='activate'),
-    path('stream_creation', StreamCreation.as_view())
+    path('stream_creation', StreamCreationView.as_view())
 ]
