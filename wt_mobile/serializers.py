b432b0a2ce210c9c9882b471289ab7a886ee4a87
Tom Yordanov <tmy@Toms-MacBook-Pro.local>
2023-08-12 23:40:07 +0300

    another fixes by tmy

--- wt_mobile/serializers.py
+++ wt_mobile/serializers.py
@@ -2,13 +2,6 @@ from rest_framework import  serializers
 from .models import User, Stream, UserRoom
 
 
-class UserSerializer(serializers.ModelSerializer):
-    """User serializer, returns all fields that the user has"""
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
 class UserSerializerSearchByUsername(serializers.ModelSerializer):
     """User serializer used to return username only"""
     class Meta:
--- wt_mobile/urls.py
+++ wt_mobile/urls.py
@@ -1,13 +1,10 @@
-# Spring-like request mappings for djnago. All urls are registered in the project url.py
-
 from wt_mobile.views import StreamView, RoomView, UserView
 from django_request_mapping import UrlPattern
 from .backend_logic import activate
 from django.urls import path
 
-urlpatterns = UrlPattern()
 
+urlpatterns = UrlPattern()
 urlpatterns.register(UserView)
 urlpatterns.register(StreamView)
 urlpatterns.register(RoomView)
-
